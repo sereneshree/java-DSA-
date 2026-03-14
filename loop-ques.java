ae4e43ecbf868d1494f8700bef513ee055a3af7c
sereneshree <mishu.08nov23@gmail.com>
2026-03-14 19:51:42 +0530

    Create loop-ques.java

---
+++ loop-ques.java
@@ -0,0 +1,9 @@
+// to print no. from 1 to 10.
+class Main {
+    public static void main(String[] args) {
+        
+        for (int i=1;i<=10;i++){
+            System.out.println(i);
+        }
+    }
+}
