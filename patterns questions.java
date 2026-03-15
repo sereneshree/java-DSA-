2cff63214fc16fb197a34af27917d3dfeffce5fa
sereneshree <mishu.08nov23@gmail.com>
2026-03-16 03:12:55 +0530

    Update patterns questions.java

--- patterns questions.java
+++ patterns questions.java
@@ -246,7 +246,37 @@ class Main {
     }
 }    
 
-   
+// to print
+
+1
+01
+101
+0101
+10101
+
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args) {
+        
+        Scanner sc=new Scanner (System.in);
+        System.out.println("enter a no");
+        int n=sc.nextInt();
+        
+        for (int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                if((i+j)%2==0){
+                      System.out.print(1);
+                }
+                else{
+                    System.out.print(0);
+                }
+            }
+         System.out.println();
+        }
+    }
+}
+
+
 
    
   
